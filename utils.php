43a9e8ef33232bf4e6e18e4eeee22fcf2b336352
benyogit <ben14687@gmail.com>
2020-01-30 11:19:02 +0200

    aa

--- utils.php
+++ utils.php
@@ -4,8 +4,8 @@ function write_to_log($title, $text){
 
         $myfile = fopen("log.txt", "w") or die("Unable to open file!");
         $txt = "John Doe\n";
-        fwrite()
-//TODO:....
+        fwrite();
+
 
 
 
